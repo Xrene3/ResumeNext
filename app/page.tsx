7f5817f3951f3a85734f0cdc2edf9d9ed13e6a75
Xrene3 <ryanclarkgeneveo03@gmail.com>
2026-03-29 17:25:44 +0800

    Temp placeholder data for home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center">
-      Yoyo
+    <div className="flex flex-col flex-1 items-center">
+      <div className="max-w-6xl card w-full bg-base-100 p-5">
+        Hello my name is jef
+      </div>
     </div>
   );
 }
